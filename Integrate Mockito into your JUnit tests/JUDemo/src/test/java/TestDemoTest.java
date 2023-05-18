8dbd634803d0ad1b815ff1dd1ef19db7f45e2d69
MrNineMan <bearth2501@yahoo.com>
2023-05-18 21:38:45 +0200

    TestDemoService Changes

--- Integrate Mockito into your JUnit tests/JUDemo/src/test/java/TestDemoTest.java
+++ Integrate Mockito into your JUnit tests/JUDemo/src/test/java/TestDemoTest.java
@@ -13,16 +13,15 @@ public class TestDemoTest {
     @Test
     @DisplayName("Correct date format")
     public void returnFormattedDate() {
-
-        
-
         LocalDateTime ldt = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         TestDemoService testDemoService = Mockito.mock(TestDemoService.class);
+        TestDemo testDemo = new TestDemo();
 
         Mockito.when(testDemoService.returnFormattedDate(ldt)).thenReturn("01/05/2023"); //Hardcode the date
-        String formattedDate = testDemoService.returnFormattedDate(ldt);
-        assertEquals(formattedDate,dateTimeFormatter.format(ldt));
+        testDemo.setTestDemoService(testDemoService);
+
+        assertEquals(testDemo.returnFormattedDate(ldt),dateTimeFormatter.format(ldt));
     }
 
 }
