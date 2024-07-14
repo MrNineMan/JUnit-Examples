72ae2a8c55631cd97dcd1ea37e113eaf68463b4f
MrNineMan <108700682+MrNineMan@users.noreply.github.com>
2024-07-14 13:05:49 +0200

    Update TestDemo.java

--- Create Basic Unit/JUDemo/src/main/java/org/example/TestDemo.java
+++ Create Basic Unit/JUDemo/src/main/java/org/example/TestDemo.java
@@ -4,6 +4,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 public class TestDemo {
     public static String returnFormattedDate(LocalDateTime ldt) {
+        // Simplified Date Format
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         return dtf.format(ldt);
